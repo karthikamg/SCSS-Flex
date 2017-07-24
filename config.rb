9044d1b0760ba5db2acde269feae94fda5f9a6e5
karthika <karthikam@qburst.com>
2017-07-24 15:38:12 +0530

    SASS-advanced: Implementation of advanced SASS

---
+++ config.rb
@@ -0,0 +1,16 @@
+#Folder Settings
+http_path = '/'
+css_dir = 'css/'
+sass_dir = 'resources/scss'
+images_dir = 'resources/images'
+javascripts_dir = 'js/'
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = true
+
+# You can select your preferred output style here ( :expanded or :compressed )
+output_style = :expanded
+
+# Obviously
+preferred_syntax = :scss
